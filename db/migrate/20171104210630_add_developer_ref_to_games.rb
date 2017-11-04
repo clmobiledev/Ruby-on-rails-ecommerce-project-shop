b9ec09f5a78fc60babdaf3648874baf7c51bf7eb
lesrordev <lesliecooper691@gmail.com>
2017-11-04 21:07:04 +0000

    Added association between developer and games.

---
+++ db/migrate/20171104210630_add_developer_ref_to_games.rb
@@ -0,0 +1,5 @@
+class AddDeveloperRefToGames < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :games, :developer, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104205613) do
+ActiveRecord::Schema.define(version: 20171104210630) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -73,6 +73,8 @@ ActiveRecord::Schema.define(version: 20171104205613) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "developer_id"
+    t.index ["developer_id"], name: "index_games_on_developer_id"
   end
 
   create_table "genres", force: :cascade do |t|
