fda5e4909b862da4fed655419579140e9d60b9ec
lesrordev <lesliecooper691@gmail.com>
2017-11-04 18:52:07 +0000

    Added a genre model and migrated the database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104185109) do
+ActiveRecord::Schema.define(version: 20171104185140) do
 
   create_table "developers", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 20171104185109) do
     t.decimal "price"
     t.integer "stock_quantity"
     t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
