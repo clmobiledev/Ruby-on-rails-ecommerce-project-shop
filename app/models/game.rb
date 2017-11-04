a8f91bb09ea0425daef8bccb823d94cb524c193f
lesrordev <lesliecooper691@gmail.com>
2017-11-04 19:52:11 +0000

    Added game validations.

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,8 @@ class Game < ApplicationRecord
   has_many :orders
   belongs_to :developer
   belongs_to :genre
+  
+  validates :title, :stock_quantity, :price, presence: true
+  validates :price, :stock_quantity, numericality: true
+  validates :name, uniqueness: true
 end
