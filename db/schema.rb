5a405acdb501d1e0ef5c92de6a0d4d56d7c61b33
lesrordev <lesliecooper691@gmail.com>
2017-11-04 23:26:00 +0000

    Added an about model and migrated the database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104211000) do
+ActiveRecord::Schema.define(version: 20171104232523) do
+
+  create_table "abouts", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
