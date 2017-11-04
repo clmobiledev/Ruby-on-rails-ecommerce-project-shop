6832e7a81e907fcc1a4b2413c7c687db370a18c9
lesrordev <lesliecooper691@gmail.com>
2017-11-04 21:09:41 +0000

    Added association between customer and orders.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104210819) do
+ActiveRecord::Schema.define(version: 20171104210909) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -92,6 +92,8 @@ ActiveRecord::Schema.define(version: 20171104210819) do
     t.decimal "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "customer_id"
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
   create_table "provinces", force: :cascade do |t|
