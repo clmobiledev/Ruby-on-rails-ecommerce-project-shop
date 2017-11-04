948c4bec3a5a0dbf2cd9d5322b3e007c6dd0dcd8
lesrordev <lesliecooper691@gmail.com>
2017-11-04 22:47:15 +0000

    Added to the game index view to loop through all the games.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,6 @@
 class GamesController < ApplicationController
   def index
-    @games = Game.order(:name)
+    @games = Game.order(:title)
   end
 
   def show
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
+  ## I modified this code to false to speed up the server response time(?)
   config.assets.debug = true
 
   # Suppress logger output for asset requests.
@@ -50,6 +51,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
+  ## I modified this code
   config.file_watcher = ActiveSupport::FileUpdateChecker
   config.reload_classes_only_on_change = false
 end
