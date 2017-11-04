d9f32141927004d5c496a727594caba520ee908a
lesrordev <lesliecooper691@gmail.com>
2017-11-04 19:54:54 +0000

    Added a customer model and migrated the database.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
   has_many :orders
   belongs_to :province
+  
+  validates :first_name, :last_name, :address, :city, :postal_code, :country, presence: true
+  validates :credit_card_number, :credit_card_expiry_date, numericality: true
 end
