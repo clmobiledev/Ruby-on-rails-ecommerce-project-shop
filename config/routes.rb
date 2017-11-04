a05466d98f7d585bd00ade40ffe2f0636d4f099c
lesrordev <lesliecooper691@gmail.com>
2017-11-04 22:37:06 +0000

    Added routes, defined their actions and constraints.

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
 
   get 'home/index'
 
+  root to: 'home#index'
+	resources :games, only [:index, :show]
+  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
