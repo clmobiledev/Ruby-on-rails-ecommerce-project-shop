c92ce31e3e6ca7b622b856f8606558aa54d21ea5
lesrordev <lesliecooper691@gmail.com>
2017-11-04 18:51:35 +0000

    Added a developer model and migrated the database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104185037) do
+ActiveRecord::Schema.define(version: 20171104185109) do
+
+  create_table "developers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "games", force: :cascade do |t|
     t.string "title"
