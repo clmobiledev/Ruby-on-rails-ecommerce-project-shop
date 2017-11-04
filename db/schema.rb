011723a497f9959a1ac01c506fe83d85264caac7
lesrordev <lesliecooper691@gmail.com>
2017-11-04 23:26:45 +0000

    Added a contact model and migrated the database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104232523) do
+ActiveRecord::Schema.define(version: 20171104232605) do
 
   create_table "abouts", force: :cascade do |t|
     t.text "text"
@@ -47,6 +47,12 @@ ActiveRecord::Schema.define(version: 20171104232523) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "customers", force: :cascade do |t|
